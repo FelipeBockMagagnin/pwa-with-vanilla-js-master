2935af787ae871791b7907463680ff56a3798c6c
Felipe Bock Magagnin <felipe.bock@hotmail.com>
2020-09-23 08:01:51 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,64 @@
+const container = document.querySelector(".container");
+const coffees = [
+  {
+    name: "Prefeitura 1",
+    image: "images/logo.svg"
+  },
+  {
+    name: "Prefeitura 1",
+    image: "images/logo.svg"
+  },
+  {
+    name: "Prefeitura 1",
+    image: "images/logo.svg"
+  },
+  {
+    name: "Prefeitura 1",
+    image: "images/logo.svg"
+  },
+  {
+    name: " Prefeitura 1",
+    image: "images/logo.svg"
+  },
+  {
+    name: " Prefeitura 1",
+    image: "images/logo.svg"
+  },
+  {
+    name: "Prefeitura 1",
+    image: "images/logo.svg"
+  },
+  {
+    name: "Prefeitura 1",
+    image: "images/logo.svg"
+  },
+  {
+    name: "Prefeitura 1",
+    image: "images/logo.svg"
+  }
+];
+const showCoffees = () => {
+  let output = "";
+  coffees.forEach(
+    ({ name, image }) =>
+      (output += `
+              <div class="card">
+                <img class="card--avatar" src=${image} />
+                <h1 class="card--title">${name}</h1>
+                <a class="card--link" target='_blank' href="http://demo.nfse-tecnos.com.br/">Acessar Cliente</a>
+              </div>
+              `)
+  );
+  container.innerHTML = output;
+};
+
+document.addEventListener("DOMContentLoaded", showCoffees);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
+  });
+}
